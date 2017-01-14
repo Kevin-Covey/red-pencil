fb78219e23a024430d2e269a1ee5a8ec842f6589
Kevin Covey <entropist65@gmail.com>
2017-01-13 21:29:37 -0600

    pulling time zone into a constant

--- src/test/groovy/com/quaildev/pillar/RedPencilTest.groovy
+++ src/test/groovy/com/quaildev/pillar/RedPencilTest.groovy
@@ -9,18 +9,20 @@ import java.time.ZoneId
 
 class RedPencilTest extends Specification {
 
+    static final ZoneId TIME_ZONE = ZoneId.of('UTC')
+
     def 'constructing a red pencil sets the price and the date of last price change to now()'() {
         given:
         Clock mockClock = Mock()
         mockClock.instant() >> Instant.now()
-        mockClock.zone >> ZoneId.of('UTC')
+        mockClock.zone >> TIME_ZONE
 
         when:
         def redPencil = new RedPencil(6.29, mockClock)
 
         then:
         redPencil.price == 6.29
-        redPencil.dateOfLastPriceChange == LocalDate.now(ZoneId.of('UTC'))
+        redPencil.dateOfLastPriceChange == LocalDate.now(TIME_ZONE)
     }
 
 }
